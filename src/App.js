33a827fbda9f15dba52a6f4ad5936cb9f49a0f0e
Samanthap803b <samanthap803b@gmail.com>
2022-01-05 11:39:52 -0800

    Psuedo code

--- src/App.js
+++ src/App.js
@@ -6,38 +6,38 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      board: ["", "", "", "", "", "", "", "", ""],
-      player: null,
-      player1:["", "", "", "", ""],
-      player2:["", "", "", "", ""]
+      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
   }
-  componentDidMount(){
-    let treasure = Math.floor(Math.random()*this.state.board.length)
-    this.setState({player: treasure})
-  }
-// we need a way to have X and O change turns.  
-  handleGamePlay = (index) => {
-    const { board, player } = this.state
-    if(index === player){
-      board[index]="❌"
-      this.setState({board: board})
-    }else{
-    board[index]="⭕️"
-    this.setState({board: board})
-    }
-    
-  }
+
+  // We need to create a function that will allow us to have an x and o by using.
+  //true and false and if statement and take turns.
+  //We are going to make a counter and from there make player 1 and if player 1 is grater thatn player2
+  //then player 2 click and after that if amount is the same than player1 goes next and so on.    
+
+
+// we need a way to have X and O change turns.
+  // handleGamePlay = (index) => {
+  //   const { board, player } = this.state
+  //   if(index === ""){
+  //     board[index]="❌"
+  //     this.setState({board: board})
+  //   }else{
+  //   board[index]="⭕️"
+  //   this.setState({board: board})
+  //   }
+  //
+  // }
 
   render(){
     return(
       <>
         <h1>Raul's and Samantha's Tic-Tac-Toe</h1>
-        <div className="gameboard">
-          {this.state.board.map((value, index) =>{
+        <div className="squares">
+          {this.state.squares.map((value, index) =>{
             return (
-              <Square 
-              value={value} 
+              <Square
+              value={value}
               key={index}
               index={index}
               handleGamePlay={this.handleGamePlay}/>
