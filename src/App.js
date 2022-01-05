603b5e7655e0c60e4c6321e3e8e697e63059033c
Echo5Mike <1raul.marquez@gmail.com>
2022-01-05 13:45:09 -0800

    we got it to take turns

--- src/App.js
+++ src/App.js
@@ -6,16 +6,30 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      turn: 0,
+        
     }
   }
+    
 
   // We need to create a function that will allow us to have an x and o by using.
   //true and false and if statement and take turns.
   //We are going to make a counter and from there make player 1 and if player 1 is grater thatn player2
   //then player 2 click and after that if amount is the same than player1 goes next and so on.    
 
+  handleGamePlay = (index) =>{
+    const {squares} = this.state
+      squares[index]="⭕️"
+      this.setState({squares: squares, turn: this.state.turn +1})
 
+    if (this.state.turn % 2 === 0){
+
+      squares[index]="❌"
+      this.setState({squares: squares, turn: this.state.turn +1})
+    }
+
+  }
 // we need a way to have X and O change turns.
   // handleGamePlay = (index) => {
   //   const { board, player } = this.state
